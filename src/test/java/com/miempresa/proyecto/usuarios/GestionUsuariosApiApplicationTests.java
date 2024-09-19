bb8fbf4bff0304cf2a6d7c462cd4b4c7ddb67a44
Abel <abelcontrerasarteaga3@gmail.com>
2024-09-19 12:14:45 -0500

    V1

---
+++ src/test/java/com/miempresa/proyecto/usuarios/GestionUsuariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miempresa.proyecto.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUsuariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
