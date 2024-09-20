d77c4b30d84791e073acba01fcdc6e5eef283175
Abel <abelcontrerasarteaga3@gmail.com>
2024-09-20 09:57:28 -0500

    diagrama

--- src/main/java/com/miempresa/proyecto/usuarios/config/AppConfig.java
+++ src/main/java/com/miempresa/proyecto/usuarios/config/AppConfig.java
@@ -3,7 +3,6 @@ package com.miempresa.proyecto.usuarios.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import org.springframework.web.client.RestTemplate;
